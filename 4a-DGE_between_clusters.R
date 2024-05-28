941b9f76edb6df5dfc0669f49fd2b3edfbfad930
Noé Perron <107487325+Noeperron@users.noreply.github.com>
2024-05-28 11:27:55 -0400

    4a-DGE_between_clusters.R

--- 4a-DGE_between_clusters.R
+++ 4a-DGE_between_clusters.R
@@ -1,5 +1,7 @@
 set.seed(3112)
 
+datasets.combined <- readRDS("rds_files/D8_Nodoublets_WithoutChloroplasts_0.55.rds") 
+
 #DEG between two specific clusters
 
 DEG <- FindMarkers(datasets.combined, ident.1 = "5", ident.2 = "6") #DEG analysis between clusters 5 and 6
