1f774e02a2e3f84b2e7dc7e2d99e3ebc30b56e3c
Noé Perron <107487325+Noeperron@users.noreply.github.com>
2024-05-28 11:28:43 -0400

    2b-Nb_of_cells_per_cluster.R

--- 2b-Nb_of_cells_per_cluster.R
+++ 2b-Nb_of_cells_per_cluster.R
@@ -1,5 +1,7 @@
 set.seed(3112)
 
+datasets.combined <- readRDS("rds_files/D8_Nodoublets_WithoutChloroplasts_0.55.rds")
+
 #Get number of cells per cluster and per sample of origin
 Nb_cells_per_cluster <- table(datasets.combined$orig.ident, datasets.combined@meta.data$seurat_clusters)
 
